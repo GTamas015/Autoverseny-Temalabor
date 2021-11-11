3851812edd365665342364b33d2d32d3fe0876cb
bluebird182 <79487434+bluebird182@users.noreply.github.com>
2021-11-11 12:34:33 +0100

    pontokon átmenő autó

--- Temalabor/Assets/Scripts/Enemy Controll/EnemyControll.cs
+++ Temalabor/Assets/Scripts/Enemy Controll/EnemyControll.cs
@@ -1,21 +1,76 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.AI;
 
 public class EnemyControll : MonoBehaviour
 {
-    NavMeshAgent agent;
-    public Transform target;
+    public Transform path;
+    private List<Transform> nodes;
+    private int currentNode = 0;
+
+    public float maxSteerAngle = 45f;
+    public float maxMotorTorque = 80f;
+    public float currentSpeed;
+    public float maxSpeed = 100f;
+
+    public WheelCollider wheelFL;
+    public WheelCollider wheelFR;
+
     void Start()
     {
-        agent = GetComponent<NavMeshAgent>();
-        agent.SetDestination(target.position);
+        Transform[] pathTranforms = path.GetComponentsInChildren<Transform>();
+        nodes = new List<Transform>();
+
+        for (int i = 0; i < pathTranforms.Length; i++)
+        {
+            if(pathTranforms[i] != path.transform)
+            {
+                nodes.Add(pathTranforms[i]);
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        ApplySteer();
+        Drive();
+        ChechWaypointDistance();
+    }
+
+    private void ApplySteer()
+    {
+        Vector3 relativeVector = transform.InverseTransformPoint(nodes[currentNode].position);
+        float newSteer = (relativeVector.x / relativeVector.magnitude) * maxSteerAngle;
+        wheelFL.steerAngle = newSteer;
+        wheelFR.steerAngle = newSteer;
+    }
+    
+    private void Drive()
+    {
+        currentSpeed = 2 * Mathf.PI * wheelFL.radius * wheelFL.rpm * 60 / 1000;
+
+        if (currentSpeed < maxSpeed)
+        {
+            wheelFL.motorTorque = maxMotorTorque;
+            wheelFR.motorTorque = maxMotorTorque;
+        } else
+        {
+            wheelFL.motorTorque = 0;
+            wheelFR.motorTorque = 0;
+        }
+    }
+
+    private void ChechWaypointDistance()
+    {
+        if(Vector3.Distance(transform.position, nodes[currentNode].position) < 0.5f)
+        {
+            if(currentNode == nodes.Count-1)
+            {
+                currentNode = 0;
+            } else
+            {
+                currentNode++;
+            }
+        }
     }
 }
