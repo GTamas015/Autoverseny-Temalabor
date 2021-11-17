36997a7fc59b38d933992bc8dc8d28b15a8b3d8f
turiakanita <turiak.anita@gmail.com>
2021-11-17 22:28:12 +0100

    Menu

--- Temalabor/Assets/Scripts/UI/CountDownTimer.cs
+++ Temalabor/Assets/Scripts/UI/CountDownTimer.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using System;
+using UnityEngine.SceneManagement;
 
 public class CountDownTimer : MonoBehaviour
 {
     [SerializeField] CarController car;
+    [SerializeField] Rigidbody enemyCar;
     [SerializeField] Text countdownText;
     [SerializeField] TimerText timer;
     [SerializeField]  int countTime;
--- Temalabor/Assets/Scripts/UI/ScoreText.cs
+++ Temalabor/Assets/Scripts/UI/ScoreText.cs
@@ -7,13 +7,12 @@ using UnityEngine.UI;
 public class ScoreText : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI scoreText;
-    public Button space;
     public int score = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        space.onClick.AddListener(TaskOnClick);   
+         
     }
 
     // Update is called once per frame
