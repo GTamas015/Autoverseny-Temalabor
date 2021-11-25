5d43ecb7a2296ea17511d7042918405e29a06f95
turiakanita <turiak.anita@gmail.com>
2021-11-25 13:47:52 +0100

    coin

---
+++ Temalabor/Assets/Scripts/Coin.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Coin : MonoBehaviour
+{
+    public float rotationspeed = 80;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, 0, rotationspeed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            ScoreText.score++;
+            Destroy(gameObject);
+        }
+    }
+}
--- Temalabor/Assets/Scripts/UI/ScoreText.cs
+++ Temalabor/Assets/Scripts/UI/ScoreText.cs
@@ -7,18 +7,19 @@ using UnityEngine.UI;
 public class ScoreText : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI scoreText;
-    public int score = 0;
+    public static int score;
 
     // Start is called before the first frame update
     void Start()
     {
-       
+        score = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        scoreText.text = score.ToString(); 
+        scoreText.text = score.ToString();
+
     }
 
 }
